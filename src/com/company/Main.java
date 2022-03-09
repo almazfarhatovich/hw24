877fb58c6e2a39838539b2adf5b1400b6ed46865
almazfarhatovich <janybekovalmaz29@gmail.com>
2022-03-09 20:17:29 +0600

    исправил)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,22 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Random random = new Random();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 15; i++) {
+            arrayList.add(random.nextInt(2));
+            linkedList.add(random.nextInt(2));
+        }
+
+
+        sort();
+        sort(arrayList);
+        sort(linkedList);
+    }
+
+
+    private static void sort() {
+        Random random = new Random();
         int[] array = new int[15];
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(2);
@@ -24,37 +40,16 @@ public class Main {
             }
         } while (changeCounter > 0);
         System.out.println(Arrays.toString(array));
-
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            arrayList.add(random.nextInt(2));
-            sort(arrayList);
-        }
-        Collections.sort(arrayList);
-        System.out.println(arrayList);
-
-
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 15; i++) {
-            linkedList.add(random.nextInt(2));
-
-        }
-        Collections.sort(linkedList);
-        System.out.println(linkedList);
-        sort(linkedList);
-
     }
-
-    private static void sort(ArrayList<Integer> arrayList) {
-        arrayList.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
-    }
-
+        private static void sort (ArrayList < Integer > arrayList) {
+            arrayList.sort(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o1 - o2;
+                }
+            });
+            System.out.println(arrayList);
+        }
 
     private static void sort(LinkedList<Integer> linkedList) {
         linkedList.sort(new Comparator<Integer>() {
@@ -63,14 +58,6 @@ public class Main {
                 return o1 - o2;
             }
         });
+        System.out.println(linkedList);
     }
 }
-
-
-/*
-     0 жана 1 деген сандар менен рандомно Массивди,
-     ArrayListти жана LinkedListти толтурунуз.
-     Overload методдорду тузунуз,
-     алар сортировка болгон элементтерди кайтарсын.
-     Башында 0дор жана аягында 1лер.
- */
